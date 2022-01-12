e7154837e4ba9f0b74078f3747048a41926ad260
Ben Coyne <bencoyne123@yahoo.co.uk>
2022-01-12 19:18:54 +0000

    end of step 10, all tests passing

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -6,14 +6,14 @@ describe Bookmark do
 
 			connection = PG.connect(dbname: 'bookmark_manager_test')
 
-			connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com/');")
+			connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
 			connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
-			connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com/');")
+			connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
 
 			bookmarks = Bookmark.all
 
-			expect(bookmarks).to include("http://www.google.com/")
-			expect(bookmarks).to include("http://www.makersacademy.com/")
+			expect(bookmarks).to include("http://www.google.com")
+			expect(bookmarks).to include("http://www.makersacademy.com")
 			expect(bookmarks).to include("http://www.destroyallsoftware.com")
 		end
 	end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -3,12 +3,12 @@ feature 'Viewing Bookmarks' do
     
     Bookmark.create(url: "http://www.makersacademy.com")
     Bookmark.create(url: "http://www.destroyallsoftware.com")
-    Bookmark.create(url: "http://www.google.com")
+    Bookmark.create(url: "http://www.google.com/")
     
     visit('/bookmarks')
 
     expect(page).to have_content("http://www.google.com/")
-    expect(page).to have_content("http://www.makersacademy.com/")
+    expect(page).to have_content("http://www.makersacademy.com")
     expect(page).to have_content("http://www.destroyallsoftware.com")
   end
 end
